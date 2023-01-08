b2913c382a39001d14cdffd71069193435d8eafb
SPiKaRDolRa <become_aum@hotmail.com>
2023-01-08 17:53:48 +0700

    feat: complete demo with first&nick

---
+++ src/screens/Map.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react'
+import MapView, { Polyline, Marker } from 'react-native-maps'
+
+const Map = () => {
+  const [makers, setMaker] = useState([])
+
+  const [makesIsReady, setMakesIsReady] = useState(false)
+
+  const fetchReportMarker = async () => {
+    try {
+      const response = await fetch('http://34.87.71.192/get-emgr-marks')
+      const json = await response.json()
+
+      setMaker(json)
+      setMakesIsReady(true)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchReportMarker()
+  }, [])
+
+  return (
+    <MapView
+      className="absolute top-0 left-0 right-0 bottom-0"
+      showsUserLocation={true}
+      showsMyLocationButton={false}
+      zoomEnabled={true}
+      initialRegion={{
+        latitude: 18.765347, // 18.765347
+        longitude: 99.076587, // 99.076587
+        latitudeDelta: 0.115,
+        longitudeDelta: 0.12,
+      }}>
+      <Polyline
+        coordinates={[
+          { latitude: 18.783377, longitude: 99.027215 },
+          { latitude: 18.780557, longitude: 99.041464 },
+          { latitude: 18.776888, longitude: 99.056972 },
+          { latitude: 18.771206, longitude: 99.067805 },
+          { latitude: 18.751263, longitude: 99.107743 },
+          { latitude: 18.745308, longitude: 99.119187 },
+          { latitude: 18.741451, longitude: 99.123904 },
+          { latitude: 18.739756, longitude: 99.124706 },
+        ]}
+        strokeColor="#0C71E0" // fallback for when `strokeColors` is not supported by the map-provider
+        strokeWidth={4}
+      />
+
+      {makesIsReady
+        ? makers.map(maker => {
+            return (
+              <Marker
+                key={maker.id}
+                title={maker.accident_info}
+                coordinate={{
+                  latitude: parseFloat(maker.lnt),
+                  longitude: parseFloat(maker.lng),
+                }}
+              />
+            )
+          })
+        : null}
+    </MapView>
+  )
+}
+
+export default Map
